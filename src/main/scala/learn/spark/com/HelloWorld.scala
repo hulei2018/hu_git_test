a537703f2bdb1eee1028843f7c8d5f6236e5764e
hulei <hulei_otm@163.com>
2018-10-28 20:36:16 +0800

    first-commit(第一次提交)

---
+++ src/main/scala/learn/spark/com/HelloWorld.scala
@@ -0,0 +1,10 @@
+package learn.spark.com
+
+
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+        println("Hello World")
+        val a=math.sqrt(2)
+       println("Hello World".apply(4))
+  }
+}
